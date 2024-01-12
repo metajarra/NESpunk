65596670ff32809b70636b310bc8ac94ce09ddc5
claire <c.justliketheword@gmail.com>
2024-01-11 16:25:05 -0800

    menu scrolling

--- Scripts/Buttons/start_button.gd
+++ Scripts/Buttons/start_button.gd
@@ -3,6 +3,7 @@ extends Button
 @onready var scene_manager : SceneManager = get_node("/root/scene_manager")
 
 func _ready() -> void:
+	grab_focus()
 	if pressed.is_connected(_on_pressed): return
 	pressed.connect(_on_pressed)
 
