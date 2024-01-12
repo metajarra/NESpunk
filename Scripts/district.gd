3274b7f98d12a8b05067135a4a35b0e65208174d
John Sangster <john.sangster445@gmail.com>
2024-01-11 19:13:14 -0500

    Update district.gd

--- Scripts/district.gd
+++ Scripts/district.gd
@@ -1,24 +1,24 @@
 # Struct
 class_name District
 
-enum OutpostType { COAL, WOOD, STEEL }
-var type : OutpostType
+enum DistrictType { HOUSING, SPECIAL }
+var type : DistrictType
 
-var resource_output_per_turn : Resources
-const OUTPUT_PER_TURN := 100
-func calculate_output() -> Resources:
-	var resources = Resources.new()
-	match type:
-		OutpostType.COAL:
-			resources.coal = OUTPUT_PER_TURN
-		OutpostType.WOOD:
-			resources.wood = OUTPUT_PER_TURN
-		OutpostType.STEEL:
-			resources.steel = OUTPUT_PER_TURN
-	return resources
+#var resource_output_per_turn : Resources
+#const OUTPUT_PER_TURN := 100
+#func calculate_output() -> Resources:
+	#var resources = Resources.new()
+	#match type:
+		#OutpostType.COAL:
+			#resources.coal = OUTPUT_PER_TURN
+		#OutpostType.WOOD:
+			#resources.wood = OUTPUT_PER_TURN
+		#OutpostType.STEEL:
+			#resources.steel = OUTPUT_PER_TURN
+	#return resources
 
 var resource_requirement_per_turn : Resources
-const REQUIREMENT_PER_PERSON := 5
+const ADDITIONAL_REQUIREMENT_PER_PERSON := 1
 func calculate_requirement() -> Resources:
 	var resources = Resources.new()
 	resources.coal = local_population.population * REQUIREMENT_PER_PERSON
